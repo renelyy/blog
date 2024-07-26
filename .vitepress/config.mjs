6ab00ea2dfa9f2cfb5eee0cd3111581be77cfbdf
yangtao <411192513@qq.com>
2024-07-26 09:58:31 +0800

    使用 vitepress 搭建技术文档博客

---
+++ .vitepress/config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vitepress';
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: '技术文档汇总',
+  description: '技术文档',
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/fets' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/fets' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  },
+
+  base: '/blog/',
+  srcDir: './src'
+});
